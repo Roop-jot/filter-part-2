949e142cba4844be22e22965acbabcec07c27e77
Roop-jot <100293679+Roop-jot@users.noreply.github.com>
2023-03-17 16:19:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+function setup()
+{
+
+}
+function draw()
+{
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+}
+function take_snapshot()
+{
+    save('My photo')
+}
